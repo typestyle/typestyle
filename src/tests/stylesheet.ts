896f145c2f04d037196f5930c58d7af276573352
Basarat Ali Syed <basarat@basarat.com>
2017-08-14 14:13:45 +1000

    adding tests

---
+++ src/tests/stylesheet.ts
@@ -0,0 +1,16 @@
+import { reinit, stylesheet } from '../index';
+import * as assert from 'assert';
+
+describe("stylesheet", () => {
+  it('basic support', () => {
+    reinit();
+    const styles = stylesheet({
+      warning: {
+        backgr
+      }
+    });
+
+    // assert.equal(animationName, 'fade-in_f1gwuh0p');
+  });
+
+});
