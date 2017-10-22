21fa915fd30056fe2a392103cf7a886db077e1c4
Sungwoo <sungwoo.cho.dev@gmail.com>
2017-10-23 08:35:53 +0900

    Fix `raf` For IE9 (#199)

--- src/internal/utilities.ts
+++ src/internal/utilities.ts
@@ -2,7 +2,7 @@ import * as types from '../types';
 import { Dictionary } from './formatting';
 
 /** Raf for node + browser */
-export const raf = typeof requestAnimationFrame === 'undefined' ? setTimeout : requestAnimationFrame.bind(window);
+export const raf = typeof requestAnimationFrame === 'undefined' ? (cb: () => void) => setTimeout(cb, 0) : requestAnimationFrame.bind(window);
 
 /**
  * Utility to join classes conditionally
