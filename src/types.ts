532f7864960032bcc63a3c0eb5f46785b43fe5ed
Shogun <shogun@cowtech.it>
2018-01-03 11:00:36 +0100

    Added debugName utility function.

--- src/types.ts
+++ src/types.ts
@@ -2704,7 +2704,7 @@ export interface NestedCSSProperties extends CSSProperties {
   $nest?: NestedCSSSelectors;
 
   /**
-   * A debug only (stripped in process.env.NODE_ENV !== 'production') name
+   * A debug only (stripped in process.env.NODE_ENV === 'production') name
    * Helps you figure out where the class is coming from if you care
    **/
   $debugName?: string;
